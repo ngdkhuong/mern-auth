b94c6d22064722033bc57883bb8c27526fe5a204
ngdkhuong <nguyenkhuong31072002@gmail.com>
2023-05-26 01:22:51 +0700

    upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
 
 import App from './App.jsx';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import store from './store.jsx';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomeScreen />} />
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="register" element={<RegisterScreen />} />
         </Route>,
     ),
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+        ,
+    </Provider>,
 );
