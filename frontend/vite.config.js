bda76648ebb7bd6d5734c85bc5cf7b95f51af208
ngdkhuong <nguyenkhuong31072002@gmail.com>
2023-05-28 21:10:46 +0700

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'https://mern-auth-ryan.onrender.com/',
+                target: 'http://localhost:5000/',
                 changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
     },
