16230d566707870641151f8e6b6bf4c24c1b0f20
Dang Khuong Nguyen <91656145+ngdkhuong@users.noreply.github.com>
2023-07-02 15:22:47 +0700

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,6 +4,7 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({userId}, process.env.JWT_SECRET, {
         expiresIn: "30d"
     });
+    // cấu hình cookie lưu token trên client và server
     return res.cookie("access_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
